afafcb8702428b1448dc241fa20db04333175710
Emily Ochoa <eochoa156@gmail.com>
2024-02-15 09:35:51 -0800

    win color for player and AI'

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -20,7 +20,7 @@ function startGame() {
     ogBoard = Array.from(Array(9).keys());
     for (var i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
-        cells[i].style.removeProperty('back-ground-color');
+        cells[i].style.removeProperty('background-color');
         cells[i].addEventListener('click', turnClick, false);
     }
 }
@@ -36,4 +36,25 @@ function turn (squareId, player) {
     if (gameWon) gameOver(gameWon)
 }
 
-function
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == huPlayer ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+}
